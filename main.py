c40f43a282df8ae30425e2aa18c5a0f077933395
Valentin Simioni <valentin.simioni@outlook.com>
2023-05-05 10:55:12 +0200

    Modifications : Tournament class added with attributes methods defined (get info, store info)

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from tournament_information.tournament import Tournament
 def main():
     tournament = Tournament.get_tournament_information()
     tournament.store_tournament_information()
-
+    T
 main()
